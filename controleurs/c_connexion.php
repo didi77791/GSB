08e159ab5a14524e8d55f86d0f016533f9b26e15
Deepika <appled@MacBook-Pro-de-Collaty.local>
2020-11-15 17:35:32 +0100

    first commit

---
+++ controleurs/c_connexion.php
@@ -0,0 +1,48 @@
+﻿<?php
+if (!isset($_REQUEST['action'])) {
+    $_REQUEST['action'] = 'demandeConnexion';
+}
+$action = $_REQUEST['action'];
+switch ($action) {
+    case 'demandeConnexion': {
+            include("vues/v_connexion.php");
+            break;
+        }
+    case 'valideConnexion': {
+            $login = $_REQUEST['login'];
+            $mdp = $_REQUEST['mdp'];
+            $internautes = $pdo->getInfosinternet($login, $mdp);
+
+
+            if (!is_array($internautes)) {
+                ajouterErreur("Login ou mot de passe incorrect");
+                include("vues/v_erreurs.php");
+                include("vues/v_connexion.php");
+            } elseif ($internautes['type'] == "visiteur") {
+                $type = $internautes['type'];
+                $id = $internautes['id'];
+                $nom = $internautes['nom'];
+                $prenom = $internautes['prenom'];
+                connecter($id, $nom, $prenom);
+                include("vues/v_sommaire.php");
+            } else {
+                $type = $internautes['type'];
+                $id = $internautes['id'];
+                $nom = $internautes['nom'];
+                $prenom = $internautes['prenom'];
+                connecter($id, $nom, $prenom);
+                include("vues/v_sommairecomp.php");
+            }
+        }break;
+
+    case "deconnexion": {
+            deconnecter();
+            include("vues/v_connexion.php");
+            break;
+        }
+    default : {
+            include("vues/v_connexion.php");
+            break;
+        }
+}
+?>
